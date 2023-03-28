b82f70601affa67e3521fdc09a7b4eb683b82e5c
Ahmed Attia <attia@vt.edu>
2023-03-28 09:23:14 -0500

    Added basic functionality to the test driver

--- test_sampling.py
+++ test_sampling.py
@@ -1,5 +1,30 @@
-import os, sys
 
-sys.path.append(os.path.abspath("mcmc"))
 
 # We start putting things together here
+
+import os, sys
+sys.path.append(os.path.abspath("mcmc"))
+
+# Import MCMC sampling routine
+from mcmc_sampling import create_hmc_sampler
+
+
+def sample_amazon_posterior(prob_space_size, sample_size=100, ):
+    """
+    MCMC sampling fromm the posterior
+
+    :param int prob_space_size: dimension of the probability space to sample
+    :param int sample_size: sample size
+
+    :returns: a list with entries being samples from the target posterior
+    """
+    sampler = create_hmc_sampler(
+        size=prob_space_size,
+        log_density=lambda x: -banana_potential_energy_value(x),
+        # log_density_grad=lambda x: -banana_potential_energy_gradient(x),
+    )
+    sample = sampler.sample(sample_size=sample_size, initial_state=[0, 0], )
+
+    return sample
+
+
